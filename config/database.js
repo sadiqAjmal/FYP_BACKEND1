15cf4c535db0f01617b0e797d4a4afb1172b4ae3
Sadiq Ajmal <sadiq.ajmal14@gmail.com>
2024-05-01 20:22:46 +0500

    Initial commit2

--- config/database.js
+++ config/database.js
@@ -6,7 +6,6 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-var serviceAccount = require("./serviceAccountKey.json");
 
 (function SetupDatabase() {
   const { readyState } = connection;
@@ -21,9 +20,20 @@ var serviceAccount = require("./serviceAccountKey.json");
         console.log("ERROR - Unable to connect to the database:", err)
       );
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert({
+        type: process.env.TYPE,
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_x509_CERT_URL,
+        client_x509_cert_url: process.env.CLIENT_x509_CERT_URL,
+      }),
       databaseURL: "https://smarttravelaid-default-rtdb.firebaseio.com",
     });
-    console.log("Connected to firebase");
+    console.log("Connected to Firebase");
   }
 })();
